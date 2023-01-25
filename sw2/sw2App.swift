01439a6c6fb92a9a64606759e8bf1071fa47e45c
Aravind Vattappallil <aravindvr99@gmail.com>
2023-01-25 18:08:49 -0500

    Initial Commit

---
+++ sw2/sw2App.swift
@@ -0,0 +1,17 @@
+//
+//  sw2App.swift
+//  sw2
+//
+//  Created by Aravind Vattappallil on 2023-01-25.
+//
+
+import SwiftUI
+
+@main
+struct sw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
